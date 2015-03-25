ced82807822dc8bae6aeba26bcc142a189377fe2
Francisco Rojas <josefcorojas@gmail.com>
2015-03-24 21:34:15 -0600

    added diferences between class methods and instance methods

--- lib/objects/my_class.rb
+++ lib/objects/my_class.rb
@@ -1,3 +1,29 @@
 class MyClass
 
+  # defines a class method called first_method using self
+  def self.first_method
+    # self here is the class MyClass
+    "called first_method on the #{self.name} class"
+  end
+
+  # defines a class method called second_method using the class's name
+  def MyClass.second_method
+    "called second_method on the #{self.name} class"
+  end
+
+  # defines an instance method in the MyClass class's metaclass or singleton class
+  # such method ends up being a class method of the class MyClass
+  # Remember: class methods are just Singleton Methods that live in the class’s singleton class
+  class << self
+    def third_method
+      "called third_method on the #{self.name} class"
+    end
+  end
+
+
+  # defines an instance method called first_method
+  def first_method
+    # self here is an instance of the MyClass class
+    "called first_method on an instance of the #{self.class.name} class"
+  end
 end
--- spec/my_class_spec.rb
+++ spec/my_class_spec.rb
@@ -1,7 +1,52 @@
 require 'spec_helper'
 
 describe MyClass do
-  it 'inherits from Object class' do
+  it 'all classes in ruby implicitly inherit from the Object class' do
     MyClass.ancestors.must_include Object
+  end
+
+  describe 'class methods' do
+    it 'calls first_method' do
+      MyClass.first_method.must_equal 'called first_method on the MyClass class'
+    end
+
+    it 'calls second_method' do
+      MyClass.second_method.must_equal 'called second_method on the MyClass class'
+    end
+
+    it 'calls third_method' do
+      MyClass.third_method.must_equal 'called third_method on the MyClass class'
+    end
+  end
+
+  describe 'instance methods' do
+    it 'calls first_method' do
+      MyClass.new.first_method.must_equal 'called first_method on an instance of the MyClass class'
+    end
+
+    it 'defines an instance method on the fly' do
+      obj = MyClass.new
+      # defines an instance method called second_method ONLY for the obj instance
+      def obj.second_method
+        "called second_method on an instance of the #{self.class.name} class"
+      end
+
+      obj.second_method.must_equal 'called second_method on an instance of the MyClass class'
+    end
+
+    it 'calls first_method on any instance of the MyClass class' do
+      MyClass.new.first_method.must_equal 'called first_method on an instance of the MyClass class'
+      MyClass.new.first_method.must_equal 'called first_method on an instance of the MyClass class'
+    end
+
+    it 'cannot call second_method on any instance of the MyClass class' do
+      obj = MyClass.new
+      def obj.second_method
+        "called second_method on an instance of the #{self.class.name} class"
+      end
+
+      obj.second_method.must_equal 'called second_method on an instance of the MyClass class'
+      -> { MyClass.new.second_method }.must_raise NoMethodError
+    end
   end
 end
